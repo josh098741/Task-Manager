3a0c6a23230efd6ab9e170474f419de221f11b99
josh098741 <morongemokaya@gmail.com>
2025-10-07 17:24:21 +0300

    Creation of the body

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,11 @@
 function Header(){
 
     return(
-        <div>This Is The Header</div>
+        <div className="w-full h-36 bg-gradient-to-b from-blue-500 to-white ">
+            <div>
+                <h1 className="font-bold text-3xl text-white text-center p-5">Note Taker Application</h1>
+            </div>
+        </div>
     );
 }
 
