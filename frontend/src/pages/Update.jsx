a7d7feda9f7e472d3fc7e6c4c04fc2de907b0cfe
josh098741 <morongemokaya@gmail.com>
2025-10-07 22:19:28 +0300

    Additional commit to my Notetaker

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -20,6 +20,7 @@ function Update(){
     useEffect(() => {
         const fetchNotes = async () => {
             try{
+                setLoading(true)
                 const res = await axios.get(API_URL)
                 setNote({
                     title: res.data.data.title,
@@ -27,27 +28,54 @@ function Update(){
                 })
             }catch(error){
                 console.log("There was an error in fetching the task",error)
+            }finally{
+                setLoading(false)
             }
         }
         fetchNotes()
     },[API_URL])
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.put(API_URL, note)
+            alert("Task Updated Successfuly")
+            navigate('/')
+        }catch(error){
+            alert("Failed to update tasks")
+            console.error("Failed to update tasks",error)
+        }
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setNote((prevNote) => ({...prevNote, [name]:value}))
+    }
+
     return(
         <div className="h-[600px] flex justify-center items-center">
             <div className="border w-96 h-96 flex flex-col justify-center items-center border-gray-900">
-                <form>
-                    <div className="flex flex-col gap-5">
-                        <div>
-                            <label>Title</label>
-                            <input name="title" value={note.title} type="text" className="border w-72 h-11 rounded-lg flex border-gray-900" />
-                        </div>
-                        <div>
-                            <label>Description</label>
-                            <textarea name="description" value={note.description} className="flex border w-72 h-24 border-gray-900"></textarea>
-                        </div>
-                        <button type="submit" className="border rounded border-gray-900">Update Content</button>
-                    </div>
-                </form>
+                {
+                    loading ? (
+                        <div className="">Loading</div>
+                    )
+                    : (
+                        <form onSubmit={handleSubmit}>
+                            <div className="flex flex-col gap-5">
+                                <div>
+                                    <label>Title</label>
+                                    <input onChange={handleChange} name="title" value={note.title} type="text" className="border w-72 h-11 rounded-lg flex border-gray-900" />
+                                </div>
+                                <div>
+                                    <label>Description</label>
+                                    <textarea onChange={handleChange} name="description" value={note.description} className="flex border w-72 h-24 border-gray-900"></textarea>
+                                </div>
+                                <button type="submit" className="border rounded border-gray-900">Update Content</button>
+                            </div>
+                        </form>
+                    )
+                }
+                
             </div>
         </div>
     );
