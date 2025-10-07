fc7db94cbdc99d16352e7de01128a3223fb34cd9
josh098741 <morongemokaya@gmail.com>
2025-10-07 16:50:48 +0300

    Additional commits to backend controller

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -3,7 +3,7 @@ const Note = require('../models/Note')
 const createNote = async (req,res) => {
     try{
         const note = await Note.create()
-        res.status(200).json({success: true, data: note})
+        res.status(201).json({success: true, data: note})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in creating note"})
     }
@@ -13,8 +13,9 @@ const getNotes = async (req,res) => {
     try{
         const notes = await Note.find()
         if(notes === 0){
-            res.status(400).json({})
+            res.status(400).json({success: false, message: "There were no notes in database"})
         }
+        res.status(200).json({success: true, data: notes})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in getting notes"})
     }
@@ -22,7 +23,11 @@ const getNotes = async (req,res) => {
 
 const getNote = async (req,res) => {
     try{
-
+        const note = await Note.findById(req.params.id)
+        if(!note){
+            res.status(400).json({succeess: false, message: "The note was not found"})
+        }
+        res.status(200).json({success: true, data: note})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in getting the note"})
     }
@@ -30,7 +35,11 @@ const getNote = async (req,res) => {
 
 const updateNote = async (req,res) => {
     try{
-
+        const note = await Note.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!note){
+            res.status(400).json({success: false, message: "The note to update was not found"})
+        }
+        res.status(200).json({success: true, data: note})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in updating the note"})
     }
