b1cf73ede45d6e4d93343d2854f2d1ac323da342
josh098741 <morongemokaya@gmail.com>
2025-10-07 17:55:19 +0300

    Additional commit to Home Body

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,9 +1,27 @@
-
+import React,{useState} from 'react'
 
 function Body(){
 
+    const [loading, setLoading] = useState(false)
+
     return(
-        <div>This Is The Body</div>
+        <div className="m-20 border h-96">
+            <div>
+                <p className="text-center font-bold text-xl">
+                    Take Your Notes here and view them Any time
+                    <span className="text-blue-500"> We Are Here For You</span>
+                </p>
+            </div>
+            <div className='flex justify-center'>
+                {
+                    loading ? <div className=" w-40 h-11 flex justify-between items-center p-3">
+                        <div>Loading</div>
+                        <div className='w-7 border-2 h-7 rounded-full border-l-blue-500 animate-spin'></div>
+                    </div> 
+                    : <div></div>
+                }
+            </div>
+        </div>
     );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from '../components/Header'
-
+import Body from '../components/Body'
 
 function Home(){
 
     return(
         <>
             <Header />
+            <Body />
         </>
     );
 }
