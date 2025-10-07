0e789366e257461f1158586f5decf0c722a05747
josh098741 <morongemokaya@gmail.com>
2025-10-07 16:39:58 +0300

    Additional commits

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -2,7 +2,8 @@ const Note = require('../models/Note')
 
 const createNote = async (req,res) => {
     try{
-
+        const note = await Note.create()
+        res.status(200).json({success: true, data: note})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in creating note"})
     }
@@ -10,7 +11,10 @@ const createNote = async (req,res) => {
 
 const getNotes = async (req,res) => {
     try{
-
+        const notes = await Note.find()
+        if(notes === 0){
+            res.status(400).json({})
+        }
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in getting notes"})
     }
@@ -34,7 +38,7 @@ const updateNote = async (req,res) => {
 
 const deleteNote = async (req,res) => {
     try{
-        const note = Note.findByIdAndDelete(req.params.id)
+        const note = await Note.findByIdAndDelete(req.params.id)
         if(!note){
             res.status(400).json({success: false, message: "The not to delete was note found"})
         }
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const connectdb = require('./config/db')
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
 const PORT = process.env.PORT || 5000
 const MONGO_URI = process.env.MONGO_URI
 
