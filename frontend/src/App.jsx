320a7a7f38e9572fd573c7892966c977d6548d1f
josh098741 <morongemokaya@gmail.com>
2025-10-07 20:14:38 +0300

    Additional commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path="/create" element={<Create />} />
-                <Route path="/update" element={<Update />} />
+                <Route path="/update/:id" element={<Update />} />
             </Routes>
         </div>
     );
