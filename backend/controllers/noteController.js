185ed5c452971f9503b37a34b572a4793b263cd0
josh098741 <morongemokaya@gmail.com>
2025-10-07 16:58:38 +0300

    Additional commits to cotroller

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -2,7 +2,7 @@ const Note = require('../models/Note')
 
 const createNote = async (req,res) => {
     try{
-        const note = await Note.create()
+        const note = await Note.create(req.body)
         res.status(201).json({success: true, data: note})
     }catch(error){
         res.status(500).json({success: false, message: "There was an error in creating note"})
@@ -12,12 +12,12 @@ const createNote = async (req,res) => {
 const getNotes = async (req,res) => {
     try{
         const notes = await Note.find()
-        if(notes === 0){
-            res.status(400).json({success: false, message: "There were no notes in database"})
+        if(notes.length === 0){
+           return res.status(404).json({success: false, message: "There were no notes in database"})
         }
         res.status(200).json({success: true, data: notes})
     }catch(error){
-        res.status(500).json({success: false, message: "There was an error in getting notes"})
+        res.status(500).json({success: false, message: "There was an error in retrieving notes"})
     }
 }
 
@@ -25,7 +25,7 @@ const getNote = async (req,res) => {
     try{
         const note = await Note.findById(req.params.id)
         if(!note){
-            res.status(400).json({succeess: false, message: "The note was not found"})
+            return res.status(404).json({success: false, message: "The note was not found"})
         }
         res.status(200).json({success: true, data: note})
     }catch(error){
@@ -37,7 +37,7 @@ const updateNote = async (req,res) => {
     try{
         const note = await Note.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
         if(!note){
-            res.status(400).json({success: false, message: "The note to update was not found"})
+            return res.status(404).json({success: false, message: "The note to update was not found"})
         }
         res.status(200).json({success: true, data: note})
     }catch(error){
@@ -49,7 +49,7 @@ const deleteNote = async (req,res) => {
     try{
         const note = await Note.findByIdAndDelete(req.params.id)
         if(!note){
-            res.status(400).json({success: false, message: "The not to delete was note found"})
+            return res.status(404).json({success: false, message: "The note to delete was not found"})
         }
         res.status(200).json({success: true, message: "note was deleted successfully"})
     }catch(error){
