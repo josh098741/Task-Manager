adc1650581c474ef44f369719f4ec446c6c6bd8e
josh098741 <morongemokaya@gmail.com>
2025-10-07 16:35:35 +0300

    Additional commits

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -8,13 +8,41 @@ const createNote = async (req,res) => {
     }
 }
 
-const getNotes = async (req,res) => {}
+const getNotes = async (req,res) => {
+    try{
 
-const getNote = async (req,res) => {}
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in getting notes"})
+    }
+}
 
-const updateNote = async (req,res) => {}
+const getNote = async (req,res) => {
+    try{
 
-const deleteNote = async (req,res) => {}
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in getting the note"})
+    }
+}
+
+const updateNote = async (req,res) => {
+    try{
+
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in updating the note"})
+    }
+}
+
+const deleteNote = async (req,res) => {
+    try{
+        const note = Note.findByIdAndDelete(req.params.id)
+        if(!note){
+            res.status(400).json({success: false, message: "The not to delete was note found"})
+        }
+        res.status(200).json({success: true, message: "note was deleted successfully"})
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in deleting the note"})
+    }
+}
 
 module.exports = {
     createNote,
